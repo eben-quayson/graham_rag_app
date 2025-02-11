f210b13f89c27f0f272c597b809a69c1dc302efd
Ebenezer Quayson <quaysonebenezerawudzi@gmail.com>
2025-02-11 15:27:39 +0000

    modifying gitignore

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 
 # Set up environment variable for Google API key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyD1DD5ybchbsYQiRcwyfoDzpRnmiR-gYGM"  # Replace with your actual API key
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 # Initialize the Google Generative AI model
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
